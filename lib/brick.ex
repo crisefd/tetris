20d30d2f492ff0e555370dafeed50daf93171511
Cristhian Fuertes <cristhian.fuertes@alertlogic.com>
2020-01-06 16:21:34 -0500

    Finish movements

--- lib/brick.ex
+++ lib/brick.ex
@@ -1,7 +1,8 @@
 defmodule Tetris.Brick do
+  @type point :: {number, number}
   @type t :: %__MODULE__{
           name: :i | :o | :l | :z,
-          location: {number, number},
+          location: point,
           rotation: 0 | 90 | 180 | 270,
           reflection: boolean
         }
@@ -23,5 +24,32 @@ defmodule Tetris.Brick do
       rotation: [0, 90, 180, 270] |> Enum.random(),
       reflection: [true, false] |> Enum.random()
     }
+  end
+
+  defmodule Movements do
+    def down({x, y}, step), do: {x, y - step}
+    def up({x, y}, step), do: {x, y + step}
+    def left({x, y}, step), do: {x - step, y}
+    def right({x, y}, step), do: {x + step, y}
+    def rotate(degrees, step) do
+      case degrees + 90 * step do
+        360 -> 0
+        res -> res
+      end
+    end
+  end
+
+  for {name, operation, field} <- [
+        {:down, &Movements.down/2, :location},
+        {:up, &Movements.up/2, :location},
+        {:left, &Movements.left/2, :location},
+        {:right, &Movements.right/2, :location},
+        {:rotate, &Movements.rotate/2, :rotation}
+      ] do
+    @spec unquote(name)(t, integer) :: t
+
+    def unquote(name)(%__MODULE__{unquote(field) => value} = brick, step \\ 1) do
+      %{brick | unquote(field) => unquote(Macro.escape(operation)).(value, step)}
+    end
   end
 end
--- test/brick_test.exs
+++ test/brick_test.exs
@@ -1,16 +1,49 @@
 defmodule BrickTest do
   use ExUnit.Case
   doctest Tetris
-  alias Tetris.Brick
-
+  import Tetris.Brick
 
   test "new brick" do
-    brick = Brick.new()
-    assert brick == %Brick{name: :i,
-                           location: {40, 0},
-                           rotation: 0,
-                           reflection: false}
-   
+    brick = new()
+    assert brick == %Tetris.Brick{name: :i, location: {40, 0}, rotation: 0, reflection: false}
   end
 
+  test "brick up movement" do
+    brick = new() |> up()
+    assert brick.location == {40, 1}
+
+    brick = new() |> up(3)
+    assert brick.location == {40, 3}
+  end
+
+  test "brick down movement" do
+    brick = new() |> down()
+    assert brick.location == {40, -1}
+
+    brick = new() |> down(3)
+    assert brick.location == {40, -3}
+  end
+
+  test "brick left movement" do
+    brick = new() |> left()
+    assert brick.location == {39, 0}
+
+    brick = new() |> left(3)
+    assert brick.location == {37, 0}
+  end
+
+  test "brick right movement" do
+    brick = new() |> right()
+    assert brick.location == {41, 0}
+
+    brick = new() |> right(3)
+    assert brick.location == {43, 0}
+  end
+
+  test "brick rotate movement" do
+    assert (new() |> rotate()).rotation == 90
+    assert (new() |> rotate(2)).rotation == 180
+    assert (new() |> rotate(3)).rotation == 270
+    assert (new() |> rotate(4)).rotation == 0
+  end
 end
