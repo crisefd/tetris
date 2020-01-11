27a83f8fc0ee3c0b90fe49de91d01d1f06cfddfe
Cristhian Fuertes <cristhian.e.fuertes@gmail.com>
2020-01-10 22:01:07 -0500

    Adding bottom module

---
+++ lib/bottom.ex
@@ -0,0 +1,26 @@
+defmodule Tetris.Bottom do
+    alias Tetris.Shape
+    @type point :: Shape.point
+    @type shape :: Shape.t
+    @type bottom :: map
+
+    @spec merge(bottom, shape) :: bottom
+
+    def merge(bottom, shape) do
+        shape
+        |> Enum.map(fn {x, y, color} -> 
+            {{x, y}, {x, y, color}} 
+        end)
+        |> Enum.into(bottom)
+    end
+
+    @spec collides?(bottom, shape | point) :: boolean
+
+    def collides?(bottom, {x, y, _}), do: collides?(bottom, {x, y})
+
+    def collides?(bottom, {x, y}), do: !!Map.get(bottom, {x, y})
+
+    def collides?(bottom, points) when is_list(points) do
+        Enum.any?(points, &collides?(bottom, &1))
+    end
+end
